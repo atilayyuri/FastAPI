38619a4d169852032a42e1193dc2ec4d4e78ba54
Atilay Tamkan <attilay.tamkan@basf.com>
2024-01-22 20:23:19 +0100

    some post and user models

---
+++ backend/db/models/blog.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, ForeignKey
+
+from sqlalchemy.orm import relationship
+
+from backend.db.base_class import Base
+
+class Blog(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    slug = Column(String, nullable=False, unique=True)
+    content = Column(Text, nullable=False)
+    published = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow())
+    author_id = Column(Integer, ForeignKey("user.id"))
+    creator = relationship("User", back_populates="blogs")
