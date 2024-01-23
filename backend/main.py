d128935b0dc35f518f272ad9d32b42c531d1757d
Atilay Tamkan <attilay.tamkan@basf.com>
2024-01-23 19:14:05 +0100

    creating new user model, the route for the user and util for the future routes

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.base import api_router
 
 from db.session import engine
 from db.base_class import Base
+
+def include_router(app):
+    app.include_router(api_router)
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
-    create_tables()
+    include_router(app)
+ #   create_tables()
     return app
 
 app = start_application()
