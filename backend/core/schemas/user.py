6d4dd0f9bb908819b2b8ed161459d94b3d76a1f0
Atilay Tamkan <attilay.tamkan@basf.com>
2024-01-24 17:19:47 +0100

    implementing status code to the response payload and also implementing which info of the user will be shown with the response payload

--- backend/core/schemas/user.py
+++ backend/core/schemas/user.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel, EmailStr, Field
 class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=7, max_length=100)
+
+
+class ShowUser(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+        from_attributes = True
